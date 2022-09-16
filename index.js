67ff7aeb5991e9e171a079522ae278a721fbf5a5
Mitchell Jepsergon Ngetich <mitchell.ngetich@student.moringaschool.com>
2022-09-16 15:41:04 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
+
 // Your code here
+class Polygon {
+    constructor(sides) {
+        this.sides = sides;
+
+    }
+    get countSides() {
+        return this.sides.length;
+    }
+    get perimeter(){
+        let sum  = this.sides.reduce((total, currentValue)=> {return total + currentValue})
+        return sum;
+    }
+}
+class Triangle extends Polygon{
+    get isValid() {
+        let x = this.sides[0];
+        let y = this.sides[1];
+        let z = this.sides[2];
+        if( x + y < z || y + z < x || x + z < y) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+}
+class Square extends Polygon {
+    get isValid(){
+        return this.sides.every(val => val === this.sides[0]);
+    }
+        get area(){
+            return this.sides[0] * this.sides[1];
+            }
+            }
+    
